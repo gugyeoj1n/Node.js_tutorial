5062b0ddd97b9165faa57cf9452de75e558c6144
gugyeoj1n <kwooj2788@kookmin.ac.kr>
2022-04-11 00:35:38 +0900

    add user functions

--- node_tutorial/index.js
+++ node_tutorial/index.js
@@ -4,6 +4,8 @@ const port = 3000
 const bodyParser = require('body-parser')
 const { User } = require('./models/User')
 const config = require('./config/key')
+const cookieParser = require('cookie-parser')
+const { auth } = require('./middleware/auth')
 
 const mongoose = require('mongoose')
 mongoose.connect(config.mongoURI)
@@ -13,17 +15,54 @@ mongoose.connect(config.mongoURI)
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
+app.use(cookieParser())
 
 app.get('/', (req, res) => {
   res.send('This is Node.js Server with Express includes MongoDB :D')
 })
 
-app.post('/register', (req, res) => {
-  // 회원가입 시 입력되는 정보를 DB에 저장
+/// #################### user route ####################
+
+app.post('/api/users/register', (req, res) => { // 회원가입
   const user = new User(req.body)
-  user.save((err, userInfo) => {
+  user.save(( err, userInfo ) => {
     if (err) return res.json({ success: false, err })
     return res.status(200).json({ success: true })
+  })
+})
+
+app.post('/api/users/login', (req, res) => { // 로그인
+  User.findOne({ email: req.body.email }, ( err, user ) => {
+    if ( !user ) {
+      return res.json({
+        loginSuccess: false,
+        message: "No user matches this email."
+      })
+    }
+    user.comparePassword(req.body.password, ( err, isMatch ) => {
+      if ( !isMatch ) return res.json({ loginSuccess: false, message: "Wrong password."})
+      user.gerToken(( err, user ) => {
+        if (err) return res.status(400).send(err)
+        res.cookie("login_token", user.token).status(200).json({ loginSuccess: true, userId: user._id})
+      })
+    })
+  })
+})
+
+app.get('/api/users/auth', auth, (req, res) => {
+  res.status(200).json({
+    _id: req.user._id,
+    email: req.user.email,
+    role: req.user.role,
+    isAdmin: req.user.role === 0 ? false : true,
+    isAuth: true
+  })
+})
+
+app.get('/api/users/logout', auth, (req, res) => {
+  User.findOneAndUpdate({ _id: req.user._id }, { token: '' }, ( err, user ) => {
+    if ( err ) return res.json({ success: false, err })
+    return res.status(200).send({ success: true })
   })
 })
 
--- node_tutorial/models/User.js
+++ node_tutorial/models/User.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose')
 const bcrypt = require('bcrypt')
 const saltRounds = 10
-
+const jwt = require('jsonwebtoken')
 
 const userSchema = mongoose.Schema({
     name: {
@@ -47,8 +47,38 @@ userSchema.pre('save', function ( next ) {
                 next()
             })
         })
+    } else {
+        next()
     }
 })
+
+userSchema.methods.comparePassword = function(plainPassword, cb) {
+    bcrypt.compare(plainPassword, this.password, ( err, isMatch ) => {
+        if ( err ) return cb(err)
+        cb(null, isMatch)
+    })
+}
+
+userSchema.methods.gerToken = function(cb) {
+    const user = this
+    const token = jwt.sign(user._id.toHexString(), '1234')
+
+    user.token = token
+    user.save(( err, user ) => {
+        if ( err ) return cb(err)
+        cb(null, user)
+    })
+}
+
+userSchema.statics.findByToken = function(token, cb) {
+    const user = this
+    jwt.verify(token, '1234', ( err, decoded ) => {
+        user.findOne({ "_id": decoded, "token": token}, ( err, user) => {
+            if ( err ) return cb( err )
+            cb(null, user)
+        })
+    })
+}
 
 const User = mongoose.model('User', userSchema)
 
